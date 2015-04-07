ae015bcfa377235c90fb12146f797cd0d1b978f8
Leland <lsmith@ashoka.org>
2015-04-06 18:42:07 -0600

    dummy functions for door controls and light sensors

--- sensors.py
+++ sensors.py
@@ -3,8 +3,11 @@ import time
 import Adafruit_MCP9808.MCP9808 as MCP9808
 
 #pin numbers
-proximity1Pin = 23
-proximity2Pin = 24
+proximity1Pin = 20
+proximity2Pin = 16
+lightPin = 26
+doorOpenPin = 25 #check that this is accurate
+doorClosePin = 11 #same here
 
 # set pins to use GPIO numbering
 GPIO.setmode(GPIO.BCM)
@@ -13,18 +16,21 @@ GPIO.setmode(GPIO.BCM)
 #pin setup
 GPIO.setup(proximity1Pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(proximity2Pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(lightPin, GPIO.IN)
+GPIO.setup(doorOpenPin, GPIO.OUT)
+GPIO.setup(doorClosePin, GPIO.OUT)
 
 #helpful global variables
 proximity1 = 0
 proximity2 = 0
 chickenCount = 0
+doorStatus = 0
 
 #convert C to F for temp sensor
 def c_to_f(c):
         return c * 9.0 / 5.0 + 32.0
 
-
-#temp sensor
+#temp sensor creation and initialization
 sensor = MCP9808.MCP9808()
 sensor.begin()
 
@@ -45,6 +51,28 @@ def proximity2(proximity2Pin):
 	else:
 		print "Object detected at sensor 2"
 
+def lightStatus(lightPin):
+	#Read light level and return 1 for dark, 0 for light
+	return 1
+
+def closeDoor(doorClosePin):
+	#turn on door motor
+	GPIO.output(doorClosePin,1)
+	#wait a couple seconds for it to complete
+	time.sleep(2.0)
+	#turn off motor
+	GPIO.output(doorClosePin,0)
+	return None
+
+def openDoor(doorOpenPin):
+	#turn on door motor
+	GPIO.output(doorOpenPin,1)
+	#wait a couple seconds for it to complete
+	time.sleep(2.0)
+	#turn off motor
+	GPIO.output(doorOpenPin,0)
+	return None
+
 #proximity
 GPIO.add_event_detect(proximity1Pin, GPIO.BOTH, callback=proximity1, bouncetime=300)
 GPIO.add_event_detect(proximity2Pin, GPIO.BOTH, callback=proximity2, bouncetime=300)
@@ -52,7 +80,12 @@ GPIO.add_event_detect(proximity2Pin, GPIO.BOTH, callback=proximity2, bouncetime=
 
 
 while True:
-       print "."
-       time.sleep(1.0)
+	if (chickenCount == 5 && lightStatus() == 1 && doorStatus = 0)
+		closeDoor()
+		doorStatus = 1
+	if (lightStatus() == 0 && doorStatus = 1)
+		openDoor()
+		doorStatus = 0
+	time.sleep(60.0)
 
 GPIO.cleanup()
