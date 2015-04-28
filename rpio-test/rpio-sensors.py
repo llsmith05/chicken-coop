cf0fccf8337cfd2600de07c0ba42c722b896355b
Leland Smith <llsmith05@gmail.com>
2015-04-28 06:41:28 +0000

    ignore

--- rpio-test/rpio-sensors.py
+++ rpio-test/rpio-sensors.py
@@ -10,8 +10,8 @@ RPIO.setmode(RPIO.BCM)
 #GPIO.setwarnings(False)
 
 #pin setup
-#RPIO.setup(proximity1Pin, RPIO.IN, pull_up_down=RPIO.PUD_UP)
-#RPIO.setup(proximity2Pin, RPIO.IN, pull_up_down=RPIO.PUD_UP)
+RPIO.setup(proximity1Pin, RPIO.IN, pull_up_down=RPIO.PUD_UP)
+RPIO.setup(proximity2Pin, RPIO.IN, pull_up_down=RPIO.PUD_UP)
 
 #helpful global variables
 proximity1 = 0 #0 or 1 if sensor is triggered
@@ -21,43 +21,32 @@ chickenCount = 0 #current number of chickens in coop
 
 
 
-def gpio_callback(gpio_id, val):
-	print("gpio %s: %s\n" % (gpio_id, val))
+def proximity1_callback(gpio_id, val):
+	global proximity1
 	proximity1 += 1
-
-
-
-#def proximity1_callback(gpio_id, val):
-#	proximity1 += 1
-#	print("gpio %s: %s\n" % (gpio_id, val))
-#	print "proximity1: " + str(proximity1)
+	print("gpio %s: %s\n" % (gpio_id, val))
 
 def proximity2_callback(gpio_id, val):
-	#proximity2 += 1
+	global proximity2
+	proximity2 += 1
 	print("gpio %s: %s\n" % (gpio_id, val))
 	#print "proximity2: " + str(proximity2)
 
 
 
-def findthepin():
-	x = [4, 17, 18, 27, 22, 23, 24, 25, 5, 12, 6, 13, 19, 26, 16, 20, 21]
 
-	while x != []:
-		i = x.pop()
-		print i
-		RPIO.setup(i, RPIO.IN, pull_up_down=RPIO.PUD_UP)
-		RPIO.add_interrupt_callback(i, gpio_callback, threaded_callback=True, debounce_timeout_ms=100)
+RPIO.add_interrupt_callback(proximity1Pin, proximity1_callback, threaded_callback=True, debounce_timeout_ms=200)
 
+RPIO.add_interrupt_callback(proximity2Pin, proximity2_callback, threaded_callback=True, debounce_timeout_ms=200)
 
-RPIO.add_interrupt_callback(proximity2Pin, proximity2_callback, threaded_callback=True, debounce_timeout_ms=300)
 
 
 
 #add_interrupt_callback(gpio_id, callback, edge='both', pull_up_down=RPIO.PUD_OFF, threaded_callback=False, debounce_timeout_ms=None)
-print str(RPIO.sysinfo())
-print str(RPIO.version())
+#print str(RPIO.sysinfo())
+#print str(RPIO.version())
 
-#findthepin()
+
 
 while True:
 	try:
@@ -65,7 +54,7 @@ while True:
 		print "Current chicken count is " + str(chickenCount)
 		print "proximity 1: " + str(proximity1)
 		print "proximity 2: " + str(proximity2)
-		RPIO.wait_for_interrupts()
+		RPIO.wait_for_interrupts(threaded=True)
 		time.sleep(10.0)
 
 #clean up GPIO usage when you quit with ctrl+C
