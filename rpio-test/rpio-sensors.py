871142b7efcdb76a5f08c4dc267ad6fc19fdae6b
Leland Smith <llsmith05@gmail.com>
2015-04-28 05:03:20 +0000

    B+ support

--- rpio-test/rpio-sensors.py
+++ rpio-test/rpio-sensors.py
@@ -2,49 +2,58 @@ import RPIO
 import time
 
 #pin numbers
-#proximity1Pin = 20
-#proximity2Pin = 20
+proximity1Pin = 16
+proximity2Pin = 21
 
 # set pins to use GPIO numbering
 RPIO.setmode(RPIO.BCM)
 #GPIO.setwarnings(False)
 
 #pin setup
-RPIO.setup(16, RPIO.IN)
-#RPIO.setup(proximity2Pin, RPIO.IN, pull_up_down=RPIO.PUD_UP)
+RPIO.setup(proximity1Pin, RPIO.IN, pull_up_down=RPIO.PUD_UP)
+RPIO.setup(proximity2Pin, RPIO.IN, pull_up_down=RPIO.PUD_UP)
 
 #helpful global variables
 proximity1 = 0 #0 or 1 if sensor is triggered
 proximity2 = 0 #0 or 1 if sensor is triggered
 chickenCount = 0 #current number of chickens in coop
 
-#https://github.com/CorCornelisse/RPIO/commit/7c35b162ab07737db8775ef3f3c459c7e53ffffa
 
-def proximity1_callback(proximity1, val):
+
+
+def proximity1_callback(gpio_id, val):
 	proximity1 += 1
 	print("gpio %s: %s\n" % (gpio_id, val))
 	print "proximity1: " + str(proximity1)
 
+def proximity2_callback(gpio_id, val):
+	proximity2 += 1
+	print("gpio %s: %s\n" % (gpio_id, val))
+	print "proximity2: " + str(proximity2)
 
-
-RPIO.add_interrupt_callback(proximity1Pin, proximity1_callback, edge='rising', pull_up_down=RPIO.PUD_UP, threaded_callback=True, debounce_timeout_ms=200)
+x = [4, 17, 18, 27, 22, 23, 24, 25, 5, 12, 6, 13, 19, 26, 16, 20, 21]
+def findthepin():
+	while x != []:
+		i = x.pop()
+		RPIO.add_interrupt_callback(i, proximity1_callback, threaded_callback=True, debounce_timeout_ms=10)
+		RPIO.add_interrupt_callback(i, proximity2_callback, threaded_callback=True, debounce_timeout_ms=10)
 
 
 
 #add_interrupt_callback(gpio_id, callback, edge='both', pull_up_down=RPIO.PUD_OFF, threaded_callback=False, debounce_timeout_ms=None)
-
-
-
-setup()
-
+print str(RPIO.sysinfo())
+print str(RPIO.version())
+findthepin()
 while True:
 	try:
 		print "Beginning count check loop"
 		print "Current chicken count is " + str(chickenCount)
-
+		print "proximity 1: " + str(proximity1)
+		print "proximity 2: " + str(proximity2)
 		time.sleep(10.0)
 
 #clean up GPIO usage when you quit with ctrl+C
 	except KeyboardInterrupt:
-		RPIO.cleanup()
 		break
+
+RPIO.cleanup()
