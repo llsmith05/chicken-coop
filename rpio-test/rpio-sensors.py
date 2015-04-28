91d37230ac2e62260ed97cda2671464c5a298c1f
jdsanchez93 <jdsanchez2011@gmail.com>
2015-04-27 21:04:27 -0600

    h

--- rpio-test/rpio-sensors.py
+++ rpio-test/rpio-sensors.py
@@ -2,15 +2,16 @@ import RPIO
 import time
 
 #pin numbers
-proximity1Pin = 20
-proximity2Pin = 20
+#proximity1Pin = 20
+#proximity2Pin = 20
 
 # set pins to use GPIO numbering
-#RPIO.setmode(RPIO.BCM)
+RPIO.setmode(RPIO.BCM)
 #GPIO.setwarnings(False)
 
 #pin setup
-RPIO.setup(proximity1Pin, RPIO.IN)
+def setup():
+	RPIO.setup(16, RPIO.IN)
 #RPIO.setup(proximity2Pin, RPIO.IN, pull_up_down=RPIO.PUD_UP)
 
 #helpful global variables
@@ -20,10 +21,10 @@ chickenCount = 0 #current number of chickens in coop
 
 
 
-
 def proximity1_callback(proximity1, val):
 	proximity1 += 1
 	print("gpio %s: %s\n" % (gpio_id, val))
+	print "proximity1: " + str(proximity1)
 
 
 
@@ -31,15 +32,11 @@ RPIO.add_interrupt_callback(proximity1Pin, proximity1_callback, edge='rising', p
 
 
 
-
-
-
-
 #add_interrupt_callback(gpio_id, callback, edge='both', pull_up_down=RPIO.PUD_OFF, threaded_callback=False, debounce_timeout_ms=None)
 
 
 
-
+setup()
 
 while True:
 	try:
