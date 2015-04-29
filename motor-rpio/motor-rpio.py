097ad8cfdfe3bf02b24b3950b766c6e7a846fa20
jdsanchez93 <jdsanchez2011@gmail.com>
2015-04-28 18:38:28 -0600

    pwm test

--- motor-rpio/motor-rpio.py
+++ motor-rpio/motor-rpio.py
@@ -14,12 +14,29 @@ doorClosePin = 18
 ##Avoid channels 0, 1, 2, 3, 6, 7. The GPU uses 1, 3, 6, 7. The frame buffer uses 0 and the SD card uses 2.
 ##channels 4,5 okay
 ##can we just use one channel?
-##default subcycle time is 20 ms
+##default subcycle time is 20 ms, but we can change it
+dmaChannel = 4
 
 PWM.setup()
-PWM.init_channel(4, subcycle_time_us=20000)
+PWM.init_channel(dmaChannel, subcycle_time_us=20000)
 
 #RPIO.setup(doorOpenPin, RPIO.OUT)
 #RPIO.setup(doorClosePin, RPIO.OUT)
+
+def doorOpen():
+	PWM.add_channel_pulse(dmaChannel, doorOpenPin, 0, 50)
+	PWM.add_channel_pulse(dmaChannel, doorOpenPin, 100, 50)
+	time.sleep(5.0)
+	PWM.clear_channel_gpio(dmaChannel, doorOpenPin)
+
+
+while True:
+	try:
+		print "Beginning motor loop"
+		doorOpen()
+		time.sleep(10.0)
+
+	except KeyboardInterrupt:
+		break
 
 PWM.cleanup()
